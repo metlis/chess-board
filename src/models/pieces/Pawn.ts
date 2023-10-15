e585c289971cf2be3eff06dc4239bc3e494ae44b
metlis <andrei.rusov8@gmail.com>
2023-10-15 16:43:32 -0400

    pawn move options

--- src/models/pieces/Pawn.ts
+++ src/models/pieces/Pawn.ts
@@ -7,9 +7,46 @@ class Pawn extends Piece {
     super(color, cell, "p");
   }
 
+  private get movingUp(): boolean {
+    return this.color !== this.controller.board.colorOnTop;
+  }
+
   getMoveOptions(): Cell[] {
-    console.log();
-    return [];
+    const cells: Cell[] = [];
+
+    const left = this.controller.getCell([
+      this.cell.coordinate[0] + 1 * (this.movingUp ? -1 : 1),
+      this.cell.coordinate[1] - 1,
+    ]);
+    if (left && left.piece && left.piece.color !== this.color) {
+      cells.push(left);
+    }
+
+    const singleStraight = this.controller.getCell([
+      this.cell.coordinate[0] + 1 * (this.movingUp ? -1 : 1),
+      this.cell.coordinate[1],
+    ]);
+    if (singleStraight && !singleStraight.piece) {
+      cells.push(singleStraight);
+    }
+
+    const doubleStraight = this.controller.getCell([
+      this.cell.coordinate[0] + 2 * (this.movingUp ? -1 : 1),
+      this.cell.coordinate[1],
+    ]);
+    if (doubleStraight && !this.moved && !doubleStraight.piece) {
+      cells.push(doubleStraight);
+    }
+
+    const right = this.controller.getCell([
+      this.cell.coordinate[0] + 1 * (this.movingUp ? -1 : 1),
+      this.cell.coordinate[1] + 1,
+    ]);
+    if (right && right.piece && right.piece.color !== this.color) {
+      cells.push(right);
+    }
+
+    return cells;
   }
 }
 
