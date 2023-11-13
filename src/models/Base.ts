c2504ba4dd717200b6b3a53ff1e609cf32898be6
metlis <andrei.rusov8@gmail.com>
2023-11-12 23:19:16 -0500

    updated base model

--- src/models/Base.ts
+++ src/models/Base.ts
@@ -1,13 +1,16 @@
 import Board from "models/Board";
 import EventBridge from "controllers/EventBridge";
+import GameController from "controllers/GameController";
 
 class Base {
   public readonly board: Board;
   public readonly eventBridge: EventBridge;
+  public readonly gameController: GameController;
 
   constructor(board: Board) {
     this.board = board;
     this.eventBridge = this.board.eventBridge;
+    this.gameController = this.board.game.controller;
   }
 }
 
