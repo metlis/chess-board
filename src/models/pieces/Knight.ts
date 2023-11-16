b276a66f06d0f95ced93bccac4865578be09cf03
metlis <andrei.rusov8@gmail.com>
2023-11-15 22:33:38 -0500

    knight moves added

--- src/models/pieces/Knight.ts
+++ src/models/pieces/Knight.ts
@@ -10,35 +10,40 @@ class Knight extends Piece {
   public getMoveOptions(): Cell[] {
     const cells: Cell[] = [];
 
-    const columns: number[] = [];
-    if (this.cell.coordinate[1] - 1 >= 0) {
-      columns.push(this.cell.coordinate[1] - 1);
-    }
-    if (this.cell.coordinate[1] + 1 <= 7) {
-      columns.push(this.cell.coordinate[1] + 1);
-    }
-
-    columns.forEach((column) => {
-      const rows: number[] = [];
-      if (this.cell.coordinate[0] - 2 >= 0) {
-        rows.push(this.cell.coordinate[0] - 2);
+    const _ = (colOffset: number, rowOffset: number) => {
+      const columns: number[] = [];
+      if (this.cell.coordinate[1] - colOffset >= 0) {
+        columns.push(this.cell.coordinate[1] - colOffset);
       }
-      if (this.cell.coordinate[0] + 2 <= 7) {
-        rows.push(this.cell.coordinate[0] + 2);
+      if (this.cell.coordinate[1] + colOffset <= 7) {
+        columns.push(this.cell.coordinate[1] + colOffset);
       }
 
-      rows.forEach((row) => {
-        const target: Cell | null = this.board.getCell([row, column]);
-        if (
-          target &&
-          this.cell.piece &&
-          (target.piece === null ||
-            target.piece.color !== this.cell.piece.color)
-        ) {
-          cells.push(target);
+      columns.forEach((column) => {
+        const rows: number[] = [];
+        if (this.cell.coordinate[0] - rowOffset >= 0) {
+          rows.push(this.cell.coordinate[0] - rowOffset);
         }
+        if (this.cell.coordinate[0] + rowOffset <= 7) {
+          rows.push(this.cell.coordinate[0] + rowOffset);
+        }
+
+        rows.forEach((row) => {
+          const target: Cell | null = this.board.getCell([row, column]);
+          if (
+            target &&
+            this.cell.piece &&
+            (target.piece === null ||
+              target.piece.color !== this.cell.piece.color)
+          ) {
+            cells.push(target);
+          }
+        });
       });
-    });
+    };
+
+    _(1, 2);
+    _(2, 1);
 
     this.moveOptions = cells;
     return cells;
