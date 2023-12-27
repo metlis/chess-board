f9a457c3a73955fa7a796be33991c945157068e8
metlis <andrei.rusov8@gmail.com>
2023-12-27 17:28:37 -0500

    icons added

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,15 +1,28 @@
 import Board from "models/Board";
 import "styles/controls.sass";
+import { FaRotate, FaCopy } from "react-icons/fa6";
 
 export default function Controls({ board }: { board: Board }) {
   return (
     <div className="controls">
-      <button
-        className="button button--rotate"
-        onClick={board.onRotate.bind(board)}
-      >
-        Rotate
-      </button>
+      <div className="controls__row controls__row--rewind">
+        <button title="First">&lt;&lt;</button>
+        <button title="Previous">&lt;</button>
+        <button title="Next">&gt;</button>
+        <button title="Last">&gt;&gt;</button>
+      </div>
+      <div className="controls__row controls__row--actions">
+        <button
+          className="button button--rotate"
+          onClick={board.onRotate.bind(board)}
+          title="Rotate board"
+        >
+          <FaRotate />
+        </button>
+        <button title="Copy moves">
+          <FaCopy />
+        </button>
+      </div>
     </div>
   );
 }
